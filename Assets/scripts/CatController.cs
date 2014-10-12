2f6b8b399ee2905d0f9774c51dbab28ec0cbd342
Radu Dita <hades2510@yahoo.com>
2014-10-13 00:14:41 +0300

    first commit

---
+++ Assets/scripts/CatController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CatsBoxes
+{
+	public class CatController : MonoBehaviour {
+
+		public Animation animation;
+
+		// Use this for initialization
+		void Start () {
+			animation.wrapMode = WrapMode.Loop;
+
+			animation.Play ("A_run");
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			var tr = gameObject.transform.parent.transform.localPosition;
+			tr.z += 0.01f;
+			gameObject.transform.parent.transform.localPosition = tr;
+		}
+	}
+}
