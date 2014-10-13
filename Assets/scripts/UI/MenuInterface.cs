78e4b1787d72f6e386af39f6ec807318034040ee
Radu Dita <hades2510@yahoo.com>
2014-10-13 23:14:50 +0300

    added menu scene and menu

---
+++ Assets/scripts/UI/MenuInterface.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuInterface : MonoBehaviour {
+
+	public GUISkin Skin;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		var width = Screen.width;
+		var height = Screen.height;
+
+		GUI.skin = Skin;
+
+		GUI.Label (new Rect (width / 2 - 50, height / 2 - 105, 100, 30), "Cats'n'Boxz" );
+
+		if (GUI.Button (new Rect (width / 2 - 50, height / 2 - 60, 100, 30), "Play")) 
+		{
+			Application.LoadLevel( "level_1" );
+		}
+
+		GUI.Button( new Rect (width/2-50, height/2 -15, 100, 30), "Credits");
+	}
+}
